47c0e2a877b102a5a2f5994831c1b6e5cbc55104
cjvanlissa <c.j.vanlissa@uu.nl>
2022-03-24 09:56:25 +0100

    update 2022-03-24 09:56:25

--- simulation.R
+++ simulation.R
@@ -12,7 +12,7 @@ library(sn)
 
 # set conditions for simulation
 hyper_parameters <- list(
-  reps = 1:100,
+  reps = 1:2,
   es = c(1, 2.75, 3.88), # Corresponding to entropy of .24, .7 and .9 for high N
   N = c(40, 100, 300),
   maxK = 3,
@@ -46,7 +46,7 @@ tab <- foreach(rownum = 1:nrow(summarydata), .options.snow = opts, .packages = c
   filnm <- paste0("tmp", Sys.getpid())
 
   # create dataframe for each group
-  dataframe <- data.frame(x = switch(dist,
+  dat <- data.frame(x = switch(dist,
                 norm = c(rnorm(floor(N/2)), rnorm(ceiling(N/2), mean = es)),
                 sn = c(sn::rsn(floor(N/2), alpha = 2),
                        sn::rsn(ceiling(N/2), xi = es, alpha = 2)),
@@ -55,16 +55,15 @@ tab <- foreach(rownum = 1:nrow(summarydata), .options.snow = opts, .packages = c
 
   res <- createMixtures(classes = 1:maxK,
                         filename_stem = filnm,
-                        rdata = dataframe, run = 1L)
+                        rdata = dat, run = 1L)
   out <- mixtureSummaryTable(res)
-  df <- out$Classes*2 # Je schreef: Hier komt nog de vector classificatie probabilities bij; dit is K-1 (bij K classen). NB Hier klopt 2*K nu eerst wel volgens mij en dan staat het nu goed, maar wilde het wel melden...
+  df <- out$Classes*2 #
   ll <- sapply(res, function(i){i$results$summaries$LL})
   caic <- -2 * ll + N*(N+df) / (N-df-2)
-  aicc <- -2 * ll + N*(df+1) / (N-df-2) # It was: ((df-2)+.001) # Why do you use .001 and why this expression?
+  aicc <- -2 * ll + N*(df+1) / (N-df-2) # CJ: Aaah ik had een copy-paste fout gemaakt uit jouw email, waardoor er stond / (df-2). Dat werkte niet
   out <- data.frame(rownum = rownum, out[c("Classes", "AIC", "BIC", "aBIC", "Entropy", "T11_VLMR_PValue", 
                    "T11_LMR_PValue", "BLRT_PValue", "min_N", "max_N", "min_prob", 
-                   "max_prob")
-  ], df = df, ll = ll, caic = caic, aicc = aicc)
+                   "max_prob")], df = df, ll = ll, caic = caic, aicc = aicc)
   write.table(x = out, file = sprintf("./results/results%d.txt" , Sys.getpid()), 
               sep = "\t", append = TRUE, row.names = FALSE, col.names = FALSE)
 }
